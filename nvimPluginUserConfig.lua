69be6ba64acef39fad3eb3de63c0c027e1cc9132
paulodev <paulomoreirarosado@hotmail.com>
2026-01-05 22:54:48 -0300

    Send the correctly nvim user file

--- nvimPluginUserConfig.lua
+++ nvimPluginUserConfig.lua
@@ -1,38 +1,116 @@
--- AstroUI provides the basis for configuring the AstroNvim User Interface
--- Configuration documentation can be found with `:h astroui`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
+-- You can also add or configure plugins by creating files in this `plugins/` folder
+-- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
+-- Here are some examples:
 
 ---@type LazySpec
 return {
-	"AstroNvim/astroui",
-	---@type AstroUIOpts
-	opts = {
-		-- change colorscheme
-		-- colorscheme = "astrodark",
-		colorscheme = "citruszest",
-		-- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-		highlights = {
-			init = { -- this table overrides highlights in all themes
-				-- Normal = { bg = "#000000" },
-			},
-			astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-				-- Normal = { bg = "#000000" },
+
+	-- == Examples of Adding Plugins ==
+
+	"andweeb/presence.nvim",
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "BufRead",
+		config = function()
+			require("lsp_signature").setup()
+		end,
+	},
+
+	-- == Examples of Overriding Plugins ==
+
+	-- customize dashboard options
+	{
+		"folke/snacks.nvim",
+		opts = {
+			dashboard = {
+				preset = {
+					header = table.concat({
+						"╔═════════════════════════════════════════════════════════╗",
+						"║ ███╗   ██╗███████╗██████╗ ██╗   ██╗  EVA-00 SYSTEM      ║",
+						"║ ████╗  ██║██╔════╝██╔══██╗██║   ██║  MAGI INTERFACE     ║",
+						"║ ██╔██╗ ██║█████╗  ██████╔╝██║   ██║                     ║",
+						"║ ██║╚██╗██║██╔══╝  ██╔══██╗╚██╗ ██╔╝  CLASSIFIED         ║",
+						"║ ██║ ╚████║███████╗██║  ██║ ╚████╔╝   ACCESS ONLY        ║",
+						"║ ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝  ╚═══╝                       ║",
+						"╚═════════════════════════════════════════════════════════╝",
+					}, "\n"),
+					items = {
+						{
+							key = "n",
+							icon = "",
+							desc = "Launch EVA",
+							action = "Telescope find_files",
+						},
+						{
+							key = "o",
+							icon = "󰈙",
+							desc = "Retrieve Pilot Logs",
+							action = "Telescope oldfiles",
+						},
+						{
+							key = "w",
+							icon = "󰈭",
+							desc = "Angel Scanner",
+							action = "Telescope live_grep",
+						},
+						{
+							key = "'",
+							icon = "",
+							desc = "NERV Archive",
+							action = "Telescope marks",
+						},
+						{
+							key = "L",
+							icon = "",
+							desc = "Reboot Cockpit",
+							action = "SessionLoad",
+						},
+					},
+				},
 			},
 		},
-		-- Icons can be configured throughout the interface
-		icons = {
-			-- configure the loading of the lsp in the status line
-			LSPLoading1 = "⠋",
-			LSPLoading2 = "⠙",
-			LSPLoading3 = "⠹",
-			LSPLoading4 = "⠸",
-			LSPLoading5 = "⠼",
-			LSPLoading6 = "⠴",
-			LSPLoading7 = "⠦",
-			LSPLoading8 = "⠧",
-			LSPLoading9 = "⠇",
-			LSPLoading10 = "⠏",
-		},
+	},
+	-- You can disable default plugins as follows:
+	{ "max397574/better-escape.nvim", enabled = true },
+
+	-- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+	{
+		"L3MON4D3/LuaSnip",
+		config = function(plugin, opts)
+			require("astronvim.plugins.configs.luasnip")(plugin, opts) -- include the default astronvim config that calls the setup call
+			-- add more custom luasnip configuration such as filetype extend or custom snippets
+			local luasnip = require("luasnip")
+			luasnip.filetype_extend("javascript", { "javascriptreact" })
+		end,
+	},
+
+	{
+		"windwp/nvim-autopairs",
+		config = function(plugin, opts)
+			require("astronvim.plugins.configs.nvim-autopairs")(plugin, opts) -- include the default astronvim config that calls the setup call
+			-- add more custom autopairs configuration such as custom rules
+			local npairs = require("nvim-autopairs")
+			local Rule = require("nvim-autopairs.rule")
+			local cond = require("nvim-autopairs.conds")
+			npairs.add_rules(
+				{
+					Rule("$", "$", { "tex", "latex" })
+						-- don't add a pair if the next character is %
+						:with_pair(cond.not_after_regex("%%"))
+						-- don't add a pair if  the previous character is xxx
+						:with_pair(
+							cond.not_before_regex("xxx", 3)
+						)
+						-- don't move right when repeat character
+						:with_move(cond.none())
+						-- don't delete if the next character is xx
+						:with_del(cond.not_after_regex("xx"))
+						-- disable adding a newline when you press <cr>
+						:with_cr(cond.none()),
+				},
+				-- disable for .vim files, but it work for another filetypes
+				Rule("a", "a", "-vim")
+			)
+		end,
 	},
 }
